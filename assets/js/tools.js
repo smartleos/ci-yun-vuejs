8846ca4c1cad060f594f9e3f360435b1aee59515
Eason iMac <eason@e-s.tw>
2023-07-31 18:18:20 +0800

    [feat] update comment

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -1,16 +1,18 @@
-const esNotification = (type, title, message) => {
-if (document.querySelector('.es_notification')) {return false}
-const div = document.createElement('div')
-div.innerHTML = '<div class="inner"><h2>' + title + '</h2><p>' + message + '</p></div>'
-div.classList.add('esNotification')
-div.classList.add(type)
-document.body.appendChild(div)
+const esNotification = (
+        type, title, message
+    ) => {
+    if (document.querySelector('.es_notification')) {return false}
+    const div = document.createElement('div')
+    div.innerHTML = '<div class="inner"><h2>' + title + '</h2><p>' + message + '</p></div>'
+    div.classList.add('esNotification')
+    div.classList.add(type)
+    document.body.appendChild(div)
 
-setTimeout(() => {
-    const noti = document.querySelector('.esNotification')
-    noti.classList.add('out')
-    setTimeout(() => {noti.remove()}, 200)
-}, 2000)
+    setTimeout(() => {
+        const noti = document.querySelector('.esNotification')
+        noti.classList.add('out')
+        setTimeout(() => {noti.remove()}, 200)
+    }, 2000)
 }
 
 const esShareTo = (url, socialMedia) => {
