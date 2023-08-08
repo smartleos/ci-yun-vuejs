016d8b1578999ea636d9eb6e08e3c1f4a6864f84
Les <letsohyeah@gmail.com>
2023-08-08 10:56:37 +0800

    [feat] 常用表單頁面切版

---
+++ pages/download.php
@@ -0,0 +1,120 @@
+<?php include('../function.php'); ?>
+<?php include('../components/header.php'); ?>
+<style>
+    <?php include('../assets/scss/main.css'); ?>
+</style>
+<?php include('../components/bg.php'); ?>
+<main class="page-download">
+    <?php
+        $img_url="testimg/01.jpg";
+        $title="常用表單";
+        include ("../components/banner.php");
+    ?>
+    <div class="container grid">
+        <div class="page-download__wrapper col-3-11-medium">
+            <div class="page-download__content">
+                <div class="page-download__content-title">
+                    <?php 
+                        $title="常用表單";
+                        include ("../components/title.php");
+                    ?>
+                </div>
+                <ul class="page-download__content-list">
+                    <li>
+                        <p>切結書</p>
+                        <div class="page-download__content-list-btn">
+                            <?php 
+                                $tag="a";
+                                $url="https://www.google.com/";
+                                $target="_blank";
+                                $text="下載";
+                                $style="";
+                                include ("../components/button.php");
+                            ?>
+                        </div>
+                    </li>
+                    <li>
+                        <p>切結書</p>
+                        <div class="page-download__content-list-btn">
+                            <?php 
+                                $tag="a";
+                                $url="https://www.google.com/";
+                                $target="_blank";
+                                $text="下載";
+                                $style="";
+                                include ("../components/button.php");
+                            ?>
+                        </div>
+                    </li>
+                    <li>
+                        <p>切結書</p>
+                        <div class="page-download__content-list-btn">
+                            <?php 
+                                $tag="a";
+                                $url="https://www.google.com/";
+                                $target="_blank";
+                                $text="下載";
+                                $style="";
+                                include ("../components/button.php");
+                            ?>
+                        </div>
+                    </li>
+                    <li>
+                        <p>切結書</p>
+                        <div class="page-download__content-list-btn">
+                            <?php 
+                                $tag="a";
+                                $url="https://www.google.com/";
+                                $target="_blank";
+                                $text="下載";
+                                $style="";
+                                include ("../components/button.php");
+                            ?>
+                        </div>
+                    </li>
+                    <li>
+                        <p>切結書</p>
+                        <div class="page-download__content-list-btn">
+                            <?php 
+                                $tag="a";
+                                $url="https://www.google.com/";
+                                $target="_blank";
+                                $text="下載";
+                                $style="";
+                                include ("../components/button.php");
+                            ?>
+                        </div>
+                    </li>
+                    <li>
+                        <p>切結書</p>
+                        <div class="page-download__content-list-btn">
+                            <?php 
+                                $tag="a";
+                                $url="https://www.google.com/";
+                                $target="_blank";
+                                $text="下載";
+                                $style="";
+                                include ("../components/button.php");
+                            ?>
+                        </div>
+                    </li>
+                    <li>
+                        <p>切結書</p>
+                        <div class="page-download__content-list-btn">
+                            <?php 
+                                $tag="a";
+                                $url="https://www.google.com/";
+                                $target="_blank";
+                                $text="下載";
+                                $style="";
+                                include ("../components/button.php");
+                            ?>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</main>
+<?php include('../components/dev.php'); ?>
+<?php include('../components/footer.php'); ?>
