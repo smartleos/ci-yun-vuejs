6cf652d934c27a2ddd605cb98b2ad69325c96ae3
Les <letsohyeah@gmail.com>
2023-08-21 14:23:52 +0800

    [feat] 首頁服務項目新增超連結

--- components/index/serviceCard.php
+++ components/index/serviceCard.php
@@ -1,8 +1,11 @@
-<div class="service-card">
+<a
+    class="service-card"
+    href="<?php echo site_url();?>products-categories/精緻代拜組"
+>
     <figure>
         <img src="<?php echo image_dir().$img_url;?>" alt="">
     </figure>
     <h3><?php echo $title;?></h3>
     <hr>
     <p><?php echo $description;?></p>
-</div>
+</a>
