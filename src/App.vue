4daf8847d09726937f242c75e1c36c33496ab0d7
Leo Shih <smartleos@hotmail.com>
2023-11-01 17:26:53 +0800

    修正src目錄的位置。

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+      <Index />
+    </div>
+  </template>
+  
+  <script>
+  import Index from './index.vue'
+  
+  export default {
+    name: 'App',
+    components: {
+      Index
+    }
+  }
+  </script>
+  
+  <style>
+  /* 你的全局樣式 */
+  </style>
+  
